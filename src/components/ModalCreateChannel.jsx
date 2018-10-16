3880866b484da4e304435bf8adcfda2f94fd6212
Dmitry Kabanov <dkabanov6@gmail.com>
2018-10-16 22:30:38 +0500

    added namespases for modals

--- src/components/ModalCreateChannel.jsx
+++ src/components/ModalCreateChannel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import cn from 'classnames';
 import connect from '../connect';
+import Modal from './Modal';
 
 const mapStateToProps = ({ addChannelState, modalCreateChannel }) => {
   const props = { addChannelState, modalCreateChannel };
@@ -32,58 +32,38 @@ class ModalCreateChannel extends React.Component {
 
     const { isOpen } = modalCreateChannel;
 
-    const modalClass = cn({
-      modal: true,
-      fade: true,
-      show: isOpen,
-    });
-
-    const modalStyle = {
-      display: isOpen ? 'block' : 'none',
-    };
-
     return (
-      <React.Fragment>
-        <div className={modalClass} tabIndex="-1" role="dialog" style={modalStyle}>
-          <div className="modal-dialog modal-dialog-centered" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Enter new channel name</h5>
-                <button onClick={this.onCloseAddChannelModal} type="button" className="close" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <form onSubmit={handleSubmit(this.addChannel)}>
-                  <Field
-                    required
-                    name="name"
-                    component="input"
-                    type="text"
-                    className="form-control mb-3"
-                    id="newChannelName"
-                  />
-                  <button
-                    type="button"
-                    className="btn btn-secondary mr-2"
-                    onClick={this.onCloseAddChannelModal}
-                  >
-                    Close
-                  </button>
-                  <button
-                    type="submit"
-                    className="btn btn-primary"
-                    disabled={pristine || submitting}
-                  >
-                    Save changes
-                  </button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-        {isOpen && <div className="modal-backdrop fade show" />}
-      </React.Fragment>
+      <Modal isOpen={isOpen}>
+        <Modal.Header onCloseModal={this.onCloseAddChannelModal}>
+          <Modal.Title>Enter new channel name</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit(this.addChannel)}>
+            <Field
+              required
+              name="name"
+              component="input"
+              type="text"
+              className="form-control mb-3"
+              id="newChannelName"
+            />
+            <button
+              type="button"
+              className="btn btn-secondary mr-2"
+              onClick={this.onCloseAddChannelModal}
+            >
+              Close
+            </button>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={pristine || submitting}
+            >
+              Save changes
+            </button>
+          </form>
+        </Modal.Body>
+      </Modal>
     );
   }
 }
--- src/components/ModalDeleteChannel.jsx
+++ src/components/ModalDeleteChannel.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import cn from 'classnames';
 import connect from '../connect';
+import Modal from './Modal';
 
 const mapStateToProps = ({ delChannelState, modalDeleteChannel }) => {
   const props = { delChannelState, modalDeleteChannel };
@@ -23,51 +23,33 @@ class ModalCreateChannel extends React.Component {
     const { modalDeleteChannel, delChannelState } = this.props;
     const { channel, isOpen } = modalDeleteChannel;
 
-    const modalClass = cn({
-      modal: true,
-      fade: true,
-      show: isOpen,
-    });
-
     const disabled = delChannelState === 'requested';
 
-    const modalStyle = {
-      display: isOpen ? 'block' : 'none',
-    };
-
     return (
-      <React.Fragment>
-        <div className={modalClass} tabIndex="-1" role="dialog" style={modalStyle}>
-          <div className="modal-dialog modal-dialog-centered" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">{`Are you sure you want to delete the channel "${channel.name}"`}</h5>
-                <button onClick={this.onCloseDelChannelModal} type="button" className="close" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <button
-                  type="button"
-                  className="btn btn-secondary mr-2"
-                  onClick={this.onCloseDelChannelModal}
-                >
-                  Close
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-danger"
-                  onClick={this.deleteChannel(channel.id)}
-                  disabled={disabled}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        {isOpen && <div className="modal-backdrop fade show" />}
-      </React.Fragment>
+      <Modal isOpen={isOpen}>
+        <Modal.Header onCloseModal={this.onCloseDelChannelModal}>
+          <Modal.Title>
+            {`Are you sure you want to delete the channel "${channel.name}"`}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <button
+            type="button"
+            className="btn btn-secondary mr-2"
+            onClick={this.onCloseDelChannelModal}
+          >
+            Close
+          </button>
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={this.deleteChannel(channel.id)}
+            disabled={disabled}
+          >
+            Delete
+          </button>
+        </Modal.Body>
+      </Modal>
     );
   }
 }
